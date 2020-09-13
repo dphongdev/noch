8e3e23cd052b06d1e2fecdd0b6bd82db4bf92cc4
nguyenduyphongtn <nguyenduyphongtn@gmail.com>
2020-09-13 17:56:37 +0700

    change inline-style-css to styled-component

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,45 +1,46 @@
 import React from 'react';
+import styled from 'styled-components';
 export function Home(props) {
-    const inputStyle = {
-        fontFamily: '"Lucida Sans Unicode", "Lucida Grande", "sans-serif"',
-        fontSize: '15px',
-        wordWrap: 'break-word',
-        color: '#e0e0ff',
-        background: 'none',
-        resize: 'none',
-        border: '0px',
-        // height: '23px',
-        opacity: '1',
-        position: 'absolute',
-        left: '12px',
-        top: '10px',
-        verticalAlign: 'middle',
-        display: 'inline-block',
+    const Wrapper = styled.div`
+        margin-left: auto;
+        margin-right: auto;
+        text-align: center;
+    `,
+        Nickname = styled.input`
+        font-family: 'Lucida Sans Unicode', 'Lucida Grande', sans-serif;
+        font-size: 15px;
+        word-wrap: break-word;
+        color: #e0e0ff;
+        background: none;
+        resize: none;
+        border: 0px;
+        opacity: 1;
+        position: absolute;
+        left: 12px;
+        top: 10px;
+        vertical-align: middle;
+        display: inline-block;
         width: '220px',
         height: '24px'
-    },
-        divStyle = {
-            width: '244px',
-            marginTop: '34px',
-            background: 'rgb(81, 175, 170)',
-            //background: 'rgb(09, 13, 20)',
-            boxShadow: 'rgb(0, 0, 0) 0px 6px 50px',
-            opacity: 1,
-            position: 'relative',
-            height: '43px',
-            borderRadius: '29px',
-            border: '2px solid rgba(0, 0, 0, 1)',
-            overflow: 'hidden',
-            display: 'inline-block',
-        },
-        rootStyle = {
-            marginLeft: 'auto', marginRight: 'auto', textAlign: 'center'
-        }
+    `,
+        NicknameWrapper = styled.div`
+        width: 244px;
+        margin-top: 34px;
+        background:rgb(81, 175, 170);
+        box-shadow: rgb(0, 0, 0) 0px 6px 50px;
+        opacity: 1;
+        position: relative;
+        height: 43px;
+        border-radius: 29px;
+        border: 2px solid rgba(0, 0, 0, 1);
+        overflow: hidden;
+        display: inline-block;
+    `
     return (
-        <div style={rootStyle}>
-            <div class={props.class} style={divStyle}>
-                <input autoFocus id="txtUsername" placeholder="Nickname" maxLength="24" style={inputStyle} type="text"></input>
-            </div>
-        </div>
+        <Wrapper class={props.class}>
+            <NicknameWrapper>
+                <Nickname autoFocus id="txtUsername" placeholder="Nickname" maxLength="24" type="text" />
+            </NicknameWrapper>
+        </Wrapper>
     );
 }
